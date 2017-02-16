380f2903f7c761bc85ad6b73a115185f14062d7e
ClaytonTaylor <claytaylor@cox.net>
2017-02-16 17:08:43 -0600

    Clock Complete

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("JS is loading");
+
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  // console.log("Hi");
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(seconds);
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getMinutes();
+  const hourDegrees = (mins / 12 * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+};
+
+setInterval(setDate, 1000);
